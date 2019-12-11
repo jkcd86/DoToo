11090c866c87a06385a1378b3bf188e40b503f0c
Manuel Valtingojer <m.valtingojer@gasmotoren.at>
2019-12-11 16:13:24 +0100

    First commit

---
+++ DoToo/DoToo/Repositories/TodoItemRepository.cs
@@ -0,0 +1,67 @@
+﻿using DoToo.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace DoToo.Repositories
+{
+    private SQLiteAsyncConnection connection;
+
+    private async Task CreateConnection()
+    {
+        if (connection != null)
+        {
+            return;
+        }
+
+        var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        var databasePath = Path.Combine(documentPath, "TodoItems.db");
+
+        connection = new SQLiteAsyncConnection(databasePath);
+        await connection.CreateTableAsync<TodoItem>();
+
+        if (await connection.Table<TodoItem>().CountAsync() == 0)
+        {
+            await connection.InsertAsync(new TodoItem() { Title = "Welcome to DoToo" });
+        }
+
+    }
+    public class TodoItemRepository : ITodoItemRepository
+    {
+        public event EventHandler<TodoItem> OnItemAdded;
+        public event EventHandler<TodoItem> InItemUpdated;
+
+        public async Task<List<TodoItem>> GetItems()
+        {
+            await CreateConnection();
+            return await connection.Table<TodoItem>().ToListAsync();
+        }
+
+        public async Task AddItem(TodoItem item)
+        {
+            await CreateConnection();
+            await connection.InsertAsync(item);
+        }
+
+        public async Task UpdateItem(TodoItem item)
+        {
+            await CreateConnection();
+            await connection.UpdateAsync(item);
+            OnItemUpdated?.Invoke(this, item);
+        }
+
+        public async Task AddOrUpdate(TodoItem item)
+        {
+            if (item.Id == 0)
+            {
+                await AddItem(item);
+            }
+            else
+            {
+                await UpdateItem(item);
+            }
+        }
+    }
+}
